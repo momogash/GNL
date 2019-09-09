cbbc69e5274d56e537a397c602f0fc1dede23ea9
Moloko MOGASHOA <momogash@c5r7s3.wethinkcode.co.za>
2019-09-09 13:19:08 +0200

    bonus

--- get_next_line.c
+++ get_next_line.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: momogash <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/09/09 11:43:18 by momogash          #+#    #+#             */
+/*   Updated: 2019/09/09 13:10:43 by momogash         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "get_next_line.h"
 
 size_t	find_newline(char *store)
@@ -10,51 +22,50 @@ size_t	find_newline(char *store)
 	return (counter);
 }
 
-int readline(char **store, char **line)
+int		readline(char **store, char **line)
 {
 	char	*temp;
 	int		pos;
+
 	pos = 0;
 	if (ft_strchr(*store, '\n') != NULL)
 	{
 		pos = find_newline(*store);
-
 		*line = ft_strsub(*store, 0, pos);
 		temp = ft_strdup(ft_strchr(*store, '\n') + 1);
 		free(*store);
 		*store = temp;
-        if ((*store)[0] == '\0')
+		if ((*store)[0] == '\0')
 			ft_strdel(store);
-        free(*line);
 	}
 	else
 	{
 		*line = ft_strdup(*store);
 		ft_strdel(store);
 	}
-	return(1);
+	return (1);
 }
 
-int get_next_line(int fd, char **line)
+int		get_next_line(int fd, char **line)
 {
 	char		stackbuff[BUFF_SIZE + 1];
-	static char	*store;
-	long        bytes;
+	static char	*store[MAX_FD];
+	long		bytes;
 	char		*temp;
 
-	if (store == NULL)
-		store = ft_strnew(0);
+	if (store[fd] == NULL)
+		store[fd] = ft_strnew(0);
 	while ((bytes = read(fd, stackbuff, BUFF_SIZE)) > 0)
 	{
 		stackbuff[bytes] = '\0';
-		temp = ft_strjoin(store, stackbuff);
+		temp = ft_strjoin(store[fd], stackbuff);
 		free(store);
-		store = temp;
+		store[fd] = temp;
 	}
-	if	(bytes < 0 )
-		return(long)(-1);
-	else if	(bytes == 0 && ft_strlen(store) == 0)
-		return(int)(0);
+	if (bytes < 0)
+		return (long)(-1);
+	else if (bytes == 0 && ft_strlen(store[fd]) == 0)
+		return (int)(0);
 	else
-		return	(long)(readline(&store, line));
+		return (long)(readline(&store[fd], line));
 }
--- get_next_line.h
+++ get_next_line.h
@@ -1,10 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: momogash <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/09/09 12:04:59 by momogash          #+#    #+#             */
+/*   Updated: 2019/09/09 13:08:03 by momogash         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
 # define BUFF_SIZE 32
+# define MAX_FD 1024
 
-#include "Libft/libft.h"
-#include <fcntl.h>
+# include "Libft/libft.h"
+# include <fcntl.h>
 
 int	get_next_line(int fd, char **line);
 
