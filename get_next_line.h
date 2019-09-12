9763d8c8dae7a1fdab0ccae3238507cfa3bed599
Moloko MOGASHOA <momogash@c5r7s3.wethinkcode.co.za>
2019-09-12 15:21:15 +0200

    libft lowercase

--- get_next_line.h
+++ get_next_line.h
@@ -6,17 +6,17 @@
 /*   By: momogash <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/09 12:04:59 by momogash          #+#    #+#             */
-/*   Updated: 2019/09/11 12:00:43 by momogash         ###   ########.fr       */
+/*   Updated: 2019/09/12 15:06:29 by momogash         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# define BUFF_SIZE 32
+# define BUFF_SIZE 16
 # define MAX_FD 1024
 
-# include "Libft/libft.h"
+# include "libft/libft.h"
 # include <fcntl.h>
 
 int	get_next_line(int fd, char **line);
